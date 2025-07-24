4cf8e10298ded05b44397dd3d1d285751594e118
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-07-24 15:44:24 +0000

    Reviewing the data transform

--- scripts/2_data_transform.py
+++ scripts/2_data_transform.py
@@ -4,16 +4,23 @@ from pathlib import Path
 
 CHUNKSIZE = 3000000
 
-COLS_PARTNERS = [0, 2, 5]
-COLS_BUSINESS = [0, 4, 6, 10, 18]
+COLS_PARTNERS  = [0, 2, 5]
+COLS_COMPANIES = [0, 1, 4]
+COLS_BUSINESS  = [0, 3, 4, 6, 10, 18]
 
-NAMES_PARTNERS = [
+NAMES_PARTNERS  = [
     'cnpj'        ,
     'name_partner',
     'start_date'  ,
 ]
-NAMES_BUSINESS = [
+NAMES_COMPANIES = [
+    'cnpj'          ,
+    'corporate_name',
+    'capital'       ,
+]
+NAMES_BUSINESS  = [
     'cnpj'        ,
+    'branch'      ,
     'trade_name'  ,
     'closing_date',
     'opening_date',
@@ -21,8 +28,10 @@ NAMES_BUSINESS = [
 ]
 
 ROOT_DIR    = Path(__file__).resolve().parent.parent
-OUTPUT_PARTNERS = ROOT_DIR / 'data/parquet/partners.parquet'
-OUTPUT_BUSINESS = ROOT_DIR / 'data/parquet/business.parquet'
+
+OUTPUT_PARTNERS  = ROOT_DIR / 'data/parquet/partners.parquet'
+OUTPUT_COMPANIES = ROOT_DIR / 'data/parquet/companies.parquet'
+OUTPUT_BUSINESS  = ROOT_DIR / 'data/parquet/business.parquet'
 
 
 OUTPUT_PARTNERS.parent.mkdir(parents=True, exist_ok=True)
@@ -32,113 +41,176 @@ OUTPUT_BUSINESS.parent.mkdir(parents=True, exist_ok=True)
 # ==========================================================================
 # PART 1 - Generate initial partners.parquet incrementally using fastparquet
 # ==========================================================================
-first_write_partners = True
-for i in range(10):
-    file_path = ROOT_DIR / f'data/csv/socios/socios{i}.csv'
+if not OUTPUT_PARTNERS.exists():
+    first_write_partners = True
 
-    print('Reading', file_path)
+    for i in range(10):
+        file_path = ROOT_DIR / f'data/csv/socios/socios{i}.csv'
 
-    df = pd.read_csv(
-        file_path            ,
-        sep=';'              ,
-        usecols=COLS_PARTNERS,
-        names=NAMES_PARTNERS ,
-        encoding='latin-1'   ,
-        on_bad_lines='skip'  ,
-    )
-    df.dropna()
+        print('Reading', file_path)
 
-    df.cnpj       = df.cnpj.astype('int32')
-    df.start_date = df.start_date.astype('int32')
-
-    df.to_parquet(
-        OUTPUT_PARTNERS                ,
-        engine='fastparquet'           ,
-        index=False                    ,
-        append=not first_write_partners,
-    )
-
-    first_write_partners = False
-    del df
-
-
-# ==========================================================================
-# PART 2 - Generate initial business.parquet incrementally using fastparquet
-# ==========================================================================
-first_write_business = True
-for i in range(10):
-    file_path = ROOT_DIR / f'data/csv/estabelecimentos/estabelecimentos{i}.csv'
-    print('Reading', file_path)
-
-    for chunk in pd.read_csv(
-        file_path            ,
-        sep=';'              ,
-        usecols=COLS_BUSINESS,
-        names=NAMES_BUSINESS ,
-        chunksize=CHUNKSIZE  ,
-        low_memory=False     ,
-        encoding='latin-1'   ,
-        on_bad_lines='skip'  ,
-    ):
-        chunk.cep = (
-            chunk.cep
-            .fillna('0')
-            .astype(str)
-            .str.replace('-', '', regex=True)
-            .astype(float)
-            .astype('int32')
+        df = pd.read_csv(
+            file_path            ,
+            sep=';'              ,
+            usecols=COLS_PARTNERS,
+            names=NAMES_PARTNERS ,
+            encoding='latin-1'   ,
+            on_bad_lines='skip'  ,
         )
+        df.dropna()
 
-        chunk.cnpj         = chunk.cnpj.astype('int32')
-        chunk.closing_date = chunk.closing_date.astype('int32')
-        chunk.opening_date = chunk.opening_date.astype('int32')
+        df.cnpj       = df.cnpj.astype('int32')
+        df.start_date = df.start_date.astype('int32')
 
-        chunk.to_parquet(
-            OUTPUT_BUSINESS                ,
+        df.to_parquet(
+            OUTPUT_PARTNERS                ,
             engine='fastparquet'           ,
             index=False                    ,
-            append=not first_write_business,
+            append=not first_write_partners,
         )
 
-        first_write_business = False
-        del chunk
+        first_write_partners = False
+
+        del df
+else:
+    print('Skipping partners.parquet — already exists.')
+
+
+# ===========================================================================
+# PART 2 - Generate initial companies.parquet incrementally using fastparquet
+# ===========================================================================
+if not OUTPUT_COMPANIES.exists():
+    first_write_companies = True
+
+    for i in range(10):
+        file_path = ROOT_DIR / f'data/csv/empresas/empresas{i}.csv'
+        print('Reading', file_path)
+
+        for chunk in pd.read_csv(
+            file_path             ,
+            sep=';'               ,
+            usecols=COLS_COMPANIES,
+            names=NAMES_COMPANIES ,
+            chunksize=CHUNKSIZE   ,
+            low_memory=False      ,
+            encoding='latin-1'    ,
+            on_bad_lines='skip'   ,
+        ):
+            chunk.cnpj    = chunk.cnpj.astype('int32')
+            chunk.capital = (
+                chunk.capital
+                .str.replace('.',  '', regex=False)
+                .str.replace(',', '.', regex=False)
+                .astype(float)
+            )
+
+            chunk.to_parquet(
+                OUTPUT_COMPANIES                ,
+                engine='fastparquet'            ,
+                index=False                     ,
+                append=not first_write_companies,
+            )
+
+            first_write_companies = False
+
+            del chunk
+else:
+    print('Skipping companies.parquet — already exists.')
+
+
+# ==========================================================================
+# PART 3 - Generate initial business.parquet incrementally using fastparquet
+# ==========================================================================
+if not OUTPUT_BUSINESS.exists():
+    first_write_business = True
+
+    for i in range(10):
+        file_path = ROOT_DIR / f'data/csv/estabelecimentos/estabelecimentos{i}.csv'
+        print('Reading', file_path)
+
+        for chunk in pd.read_csv(
+            file_path            ,
+            sep=';'              ,
+            usecols=COLS_BUSINESS,
+            names=NAMES_BUSINESS ,
+            chunksize=CHUNKSIZE  ,
+            low_memory=False     ,
+            encoding='latin-1'   ,
+            on_bad_lines='skip'  ,
+        ):
+            chunk.branch = (
+                chunk.branch
+                .astype(str)
+                .str
+                .strip()
+                == '1'
+            )
+            chunk.cep = (
+                chunk.cep
+                .fillna('0')
+                .astype(str)
+                .str.replace('-', '', regex=True)
+                .astype(float)
+                .astype('int32')
+            )
+
+            chunk.cnpj         = chunk.cnpj.astype('int32')
+            chunk.closing_date = chunk.closing_date.astype('int32')
+            chunk.opening_date = chunk.opening_date.astype('int32')
+
+            chunk.to_parquet(
+                OUTPUT_BUSINESS                ,
+                engine='fastparquet'           ,
+                index=False                    ,
+                append=not first_write_business,
+            )
+
+            first_write_business = False
+
+            del chunk
+else:
+    print('Skipping business.parquet — already exists.')
 
 
 # ==============================================================
-# PART 3 - Reload both files and rewrite with higher compression
+# PART 4 - Reload both files and rewrite with higher compression
 # ==============================================================
-partners = pd.read_parquet(OUTPUT_PARTNERS)
+print('Rewriting parquet files with pyarrow compression...')
 
-partners.sort_values(
-    by=[
-        'start_date'  ,
-        'name_partner',
-    ],
-    inplace=True,
-)
-partners.to_parquet(
-    OUTPUT_PARTNERS ,
-    engine='pyarrow',
-    index=False     ,
-)
+if not OUTPUT_PARTNERS.exists():
+    partners = pd.read_parquet(OUTPUT_PARTNERS)
 
-del partners
+    partners.sort_values(
+        by=[
+            'start_date'  ,
+            'name_partner',
+        ],
+        inplace=True,
+    )
+    partners.to_parquet(
+        OUTPUT_PARTNERS ,
+        engine='pyarrow',
+        index=False     ,
+    )
+
+    del partners
 
 
-business = pd.read_parquet(OUTPUT_BUSINESS)
+if not OUTPUT_BUSINESS.exists():
+    business = pd.read_parquet(OUTPUT_BUSINESS)
 
-business.sort_values(
-    by=[
-        'closing_date',
-        'opening_date',
-        'cep'         ,
-    ],
-    inplace=True,
-)
-business.to_parquet(
-    OUTPUT_BUSINESS ,
-    engine='pyarrow',
-    index=False     ,
-)
+    business.sort_values(
+        by=[
+            'closing_date',
+            'opening_date',
+            'cep'         ,
+        ],
+        inplace=True,
+    )
+    business.to_parquet(
+        OUTPUT_BUSINESS ,
+        engine='pyarrow',
+        index=False     ,
+    )
 
-del business
+    del business
