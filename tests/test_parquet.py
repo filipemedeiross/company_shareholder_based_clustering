fcec97505e12d8251ae7187bfd3ba051a6769f1d
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-07-25 15:39:39 +0000

    Include tests verifying cnpj between files

--- tests/test_parquet.py
+++ tests/test_parquet.py
@@ -10,8 +10,9 @@ from fastparquet import ParquetFile
 class TestParquet(unittest.TestCase):
     ROOT_DIR = Path(__file__).resolve().parent.parent
 
-    PARTNERS_PARQUET = ROOT_DIR / 'data/parquet/partners.parquet'
-    BUSINESS_PARQUET = ROOT_DIR / 'data/parquet/business.parquet'
+    PARTNERS_PARQUET  = ROOT_DIR / 'data/parquet/partners.parquet'
+    COMPANIES_PARQUET = ROOT_DIR / 'data/parquet/companies.parquet'
+    BUSINESS_PARQUET  = ROOT_DIR / 'data/parquet/business.parquet'
 
 
     def get_sample_from_parquet(self, filename, n=3):
@@ -88,6 +89,10 @@ class TestParquet(unittest.TestCase):
 
 
     def test_partners_parquet_contains_csv_data(self):
+        print()
+        print("▶️ Starting test: test_partners_parquet_contains_csv_data...")
+        print()
+
         CNPJ_COL, NAME_COL, _ = SOCIOS_COLS = [0, 2, 5]
         SOCIOS_PATH = [
             self.ROOT_DIR / f'data/csv/socios/socios{i}.csv'
@@ -124,6 +129,10 @@ class TestParquet(unittest.TestCase):
 
 
     def test_business_parquet_contains_csv_data(self):
+        print()
+        print("▶️ Starting test: test_business_parquet_contains_csv_data")
+        print()
+
         BASIC_COL = 0
         ORDER_COL = 1
         DV_COL    = 2
@@ -169,6 +178,45 @@ class TestParquet(unittest.TestCase):
                     self.fail(
                         f"❌ CNPJ {cnpj} from parquet not found in any estabelecimentos[0-9].csv file."
                     )
+
+
+    def test_partners_subset_of_companies_and_business(self):
+        print()
+        print("▶️ Starting test: test_partners_subset_of_companies_and_business")
+        print()
+
+        cnpjs_partners  = set(pd.read_parquet(self.PARTNERS_PARQUET , columns=['cnpj']).cnpj)
+        cnpjs_companies = set(pd.read_parquet(self.COMPANIES_PARQUET, columns=['cnpj']).cnpj)
+        cnpjs_business  = set(pd.read_parquet(self.BUSINESS_PARQUET , columns=['cnpj']).cnpj)
+
+        self.assertTrue(
+            cnpjs_partners.issubset(cnpjs_companies),
+            "❌ Not all partner CNPJs are present in companies.parquet"
+        )
+        self.assertTrue(
+            cnpjs_partners.issubset(cnpjs_business),
+            "❌ Not all partner CNPJs are present in business.parquet"
+        )
+
+        print(f"✅ Percentage of partner CNPJs present in companies: {(len(cnpjs_partners) / len(cnpjs_companies)) * 100:.2f}%")
+        print(f"✅ Total CNPJs with data in business: {len(cnpjs_business)}")
+
+
+    def test_companies_equal_business(self):
+        print()
+        print("▶️ Starting test: test_companies_equal_business")
+        print()
+
+        cnpjs_companies = set(pd.read_parquet(self.COMPANIES_PARQUET, columns=['cnpj']).cnpj)
+        cnpjs_business  = set(pd.read_parquet(self.BUSINESS_PARQUET , columns=['cnpj']).cnpj)
+
+        self.assertEqual(
+            cnpjs_companies,
+            cnpjs_business ,
+            "❌ companies.parquet and business.parquet contain different CNPJs"
+        )
+
+        print("✅ CNPJs in companies and business are identical.")
 
 
 if __name__ == '__main__':
