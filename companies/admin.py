3b7229c9d7983f0259d9b608de61147b0afc593a
filipemedeiross <filipemedeiros2013@gmail.com>
2025-08-05 14:03:11 -0300

    Adapting the legacy database models for the project, e adding that in admin

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,93 @@
+from datetime import datetime
 from django.contrib import admin
 
-# Register your models here.
+from .models import Companies  , \
+                    Partners   , \
+                    Business   , \
+                    PartnersFts, \
+                    BusinessFts
+
+
+@admin.register(Companies)
+class CompaniesAdmin(admin.ModelAdmin):
+    list_display  = ('cnpj', 'corporate_name', 'capital')
+    search_fields = ('cnpj', 'corporate_name',)
+
+    list_per_page = 20
+
+
+@admin.register(Partners)
+class PartnersAdmin(admin.ModelAdmin):
+    list_display  = ('cnpj', 'name_partner', 'formatted_start_date')
+    search_fields = ('cnpj', 'name_partner',)
+
+    list_per_page = 20
+
+    def formatted_start_date(self, obj):
+        if obj.start_date:
+            try:
+                return datetime.strptime(str(obj.start_date), '%Y%m%d').date()
+            except ValueError:
+                return obj.start_date
+
+        return "-"
+
+    formatted_start_date.admin_order_field = 'start_date'
+    formatted_start_date.short_description = 'Start Date'
+
+
+@admin.register(Business)
+class BusinessAdmin(admin.ModelAdmin):
+    list_display  = (
+        'cnpj'                  ,
+        'cnpj_order'            ,
+        'cnpj_dv'               ,
+        'trade_name'            ,
+        'branch'                ,
+        'formatted_opening_date',
+        'formatted_closing_date',
+        'cep'                   ,
+    )
+    search_fields = ('cnpj', 'trade_name',)
+    list_filter   = ('branch',)
+
+    list_per_page = 20
+
+    def formatted_opening_date(self, obj):
+        if obj.opening_date:
+            try:
+                return datetime.strptime(str(obj.opening_date), '%Y%m%d').date()
+            except ValueError:
+                return obj.opening_date
+
+        return "-"
+
+    def formatted_closing_date(self, obj):
+        if obj.closing_date:
+            try:
+                return datetime.strptime(str(obj.closing_date), '%Y%m%d').date()
+            except ValueError:
+                return obj.closing_date
+
+        return "-"
+
+    formatted_opening_date.admin_order_field = 'opening_date'
+    formatted_opening_date.short_description = 'Opening Date'
+    formatted_closing_date.admin_order_field = 'closing_date'
+    formatted_closing_date.short_description = 'Closing Date'
+
+
+@admin.register(PartnersFts)
+class PartnersFtsAdmin(admin.ModelAdmin):
+    list_display  = ('name_partner',)
+    search_fields = ('name_partner',)
+
+    list_per_page = 20
+
+
+@admin.register(BusinessFts)
+class BusinessFtsAdmin(admin.ModelAdmin):
+    list_display  = ('trade_name',)
+    search_fields = ('trade_name',)
+
+    list_per_page = 20
--- companies/models.py
+++ companies/models.py
@@ -2,52 +2,69 @@ from django.db import models
 
 
 class Companies(models.Model):
-    cnpj = models.IntegerField(primary_key=True)
+    cnpj           = models.IntegerField(primary_key=True)
     corporate_name = models.TextField(blank=True, null=True)
-    capital = models.IntegerField(blank=True, null=True)
+    capital        = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed  = False
         db_table = 'companies'
+        verbose_name        = 'Companie'
+        verbose_name_plural = 'Companies'
+
+    def __str__(self):
+        return f"{self.cnpj}"
 
 
 class Partners(models.Model):
-    cnpj = models.ForeignKey(Companies, models.DO_NOTHING, db_column='cnpj')
+    rowid        = models.IntegerField(primary_key=True)
+    cnpj         = models.ForeignKey(Companies, models.DO_NOTHING, db_column='cnpj')
     name_partner = models.TextField(blank=True, null=True)
-    start_date = models.IntegerField(blank=True, null=True)
+    start_date   = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed  = False
         db_table = 'partners'
+        verbose_name        = 'Partner'
+        verbose_name_plural = 'Partners'
 
 
 class Business(models.Model):
-    cnpj = models.ForeignKey('Companies', models.DO_NOTHING, db_column='cnpj')
-    cnpj_order = models.IntegerField()
-    cnpj_dv = models.IntegerField()
-    branch = models.BooleanField(blank=True, null=True)
-    trade_name = models.TextField(blank=True, null=True)
+    rowid        = models.IntegerField(primary_key=True)
+    cnpj         = models.ForeignKey('Companies', models.DO_NOTHING, db_column='cnpj')
+    cnpj_order   = models.IntegerField()
+    cnpj_dv      = models.IntegerField()
+    branch       = models.BooleanField(blank=True, null=True)
+    trade_name   = models.TextField(blank=True, null=True)
     closing_date = models.IntegerField(blank=True, null=True)
     opening_date = models.IntegerField(blank=True, null=True)
-    cep = models.IntegerField(blank=True, null=True)
+    cep          = models.IntegerField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'business'
-        unique_together = (('cnpj', 'cnpj_order', 'cnpj_dv'))
+        unique_together = (('cnpj', 'cnpj_order', 'cnpj_dv'),)
+        verbose_name        = 'Business'
+        verbose_name_plural = 'Businesses'
 
 
 class PartnersFts(models.Model):
+    rowid        = models.IntegerField(primary_key=True)
     name_partner = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed  = False
         db_table = 'partners_fts'
+        verbose_name        = 'Partners FTS'
+        verbose_name_plural = 'Partners FTSs'
 
 
 class BusinessFts(models.Model):
+    rowid      = models.IntegerField(primary_key=True)
     trade_name = models.TextField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed  = False
         db_table = 'business_fts'
+        verbose_name        = 'Businesses FTS'
+        verbose_name_plural = 'Businesses FTSs'
