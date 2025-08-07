b1cde2dbafbf02ff380e1500fbdc0a441009faa4
filipemedeiross <filipemedeiros2013@gmail.com>
2025-08-07 15:41:32 -0300

    Refactoring the admin apresentation

--- companies/admin.py
+++ companies/admin.py
@@ -10,71 +10,36 @@ from .models import Companies  , \
 
 @admin.register(Companies)
 class CompaniesAdmin(admin.ModelAdmin):
-    list_display  = ('cnpj', 'corporate_name', 'capital')
-    search_fields = ('cnpj', 'corporate_name',)
+    list_display  = ('cnpj_str', 'corporate_name', 'capital')
+    search_fields = ('cnpj'    , 'corporate_name',)
 
     list_per_page = 20
 
 
 @admin.register(Partners)
 class PartnersAdmin(admin.ModelAdmin):
-    list_display  = ('cnpj', 'name_partner', 'formatted_start_date')
-    search_fields = ('cnpj', 'name_partner',)
+    list_display  = ('cnpj_str', 'name_partner', 'start_date_obj')
+    search_fields = ('cnpj'    , 'name_partner',)
 
     list_per_page = 20
-
-    def formatted_start_date(self, obj):
-        if obj.start_date:
-            try:
-                return datetime.strptime(str(obj.start_date), '%Y%m%d').date()
-            except ValueError:
-                return obj.start_date
-
-        return "-"
-
-    formatted_start_date.admin_order_field = 'start_date'
-    formatted_start_date.short_description = 'Start Date'
 
 
 @admin.register(Business)
 class BusinessAdmin(admin.ModelAdmin):
     list_display  = (
-        'cnpj'                  ,
-        'cnpj_order'            ,
-        'cnpj_dv'               ,
-        'trade_name'            ,
-        'branch'                ,
-        'formatted_opening_date',
-        'formatted_closing_date',
-        'cep'                   ,
+        'cnpj_str'        ,
+        'cnpj_order_str'  ,
+        'cnpj_dv_str'     ,
+        'trade_name'      ,
+        'branch'          ,
+        'opening_date_obj',
+        'closing_date_obj',
+        'cep_str'         ,
     )
-    search_fields = ('cnpj', 'trade_name',)
+    search_fields = ('cnpj'  , 'trade_name',)
     list_filter   = ('branch',)
 
     list_per_page = 20
-
-    def formatted_opening_date(self, obj):
-        if obj.opening_date:
-            try:
-                return datetime.strptime(str(obj.opening_date), '%Y%m%d').date()
-            except ValueError:
-                return obj.opening_date
-
-        return "-"
-
-    def formatted_closing_date(self, obj):
-        if obj.closing_date:
-            try:
-                return datetime.strptime(str(obj.closing_date), '%Y%m%d').date()
-            except ValueError:
-                return obj.closing_date
-
-        return "-"
-
-    formatted_opening_date.admin_order_field = 'opening_date'
-    formatted_opening_date.short_description = 'Opening Date'
-    formatted_closing_date.admin_order_field = 'closing_date'
-    formatted_closing_date.short_description = 'Closing Date'
 
 
 @admin.register(PartnersFts)
--- companies/models.py
+++ companies/models.py
@@ -1,4 +1,7 @@
+from datetime  import datetime
+
 from django.db import models
+from django.contrib import admin
 
 
 class Companies(models.Model):
@@ -9,11 +12,15 @@ class Companies(models.Model):
     class Meta:
         managed  = False
         db_table = 'companies'
-        verbose_name        = 'Companie'
+        verbose_name        = 'Company'
         verbose_name_plural = 'Companies'
 
     def __str__(self):
         return f"{self.cnpj}"
+
+    @admin.display(description='CNPJ')
+    def cnpj_str(self):
+        return f"{self.cnpj:08d}"
 
 
 class Partners(models.Model):
@@ -28,6 +35,17 @@ class Partners(models.Model):
         verbose_name        = 'Partner'
         verbose_name_plural = 'Partners'
 
+    @admin.display(description='CNPJ')
+    def cnpj_str(self):
+        return f"{self.cnpj.cnpj:08d}"
+
+    @admin.display(description='Start Date')
+    def start_date_obj(self):
+        if self.start_date:
+            return datetime.strptime(str(self.start_date), "%Y%m%d").date()
+
+        return None
+
 
 class Business(models.Model):
     rowid        = models.IntegerField(primary_key=True)
@@ -41,11 +59,44 @@ class Business(models.Model):
     cep          = models.IntegerField(blank=True, null=True)
 
     class Meta:
-        managed = False
+        managed  = False
         db_table = 'business'
         unique_together = (('cnpj', 'cnpj_order', 'cnpj_dv'),)
         verbose_name        = 'Business'
         verbose_name_plural = 'Businesses'
+
+    @admin.display(description='CNPJ')
+    def cnpj_str(self):
+        return f"{self.cnpj.cnpj:08d}"
+
+    @admin.display(description='CNPJ Order')
+    def cnpj_order_str(self):
+        return f"{self.cnpj_order:04d}"
+
+    @admin.display(description='CNPJ Dv')
+    def cnpj_dv_str(self):
+        return f"{self.cnpj_dv:02d}"
+
+    @admin.display(description='Opening Date')
+    def opening_date_obj(self):
+        if self.opening_date:
+            return datetime.strptime(str(self.opening_date), "%Y%m%d").date()
+
+        return None
+
+    @admin.display(description='Closing Date')
+    def closing_date_obj(self):
+        if self.closing_date:
+            return datetime.strptime(str(self.closing_date), "%Y%m%d").date()
+
+        return None
+
+    @admin.display(description='CEP')
+    def cep_str(self):
+        if self.cep:
+            return f"{self.cep:08d}"
+
+        return None
 
 
 class PartnersFts(models.Model):
