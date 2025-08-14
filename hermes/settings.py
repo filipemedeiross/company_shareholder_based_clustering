4d7cf75fce03a082dbb3c43078181c70a7e804c2
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-08-14 15:48:56 +0000

    Improving the capital printing

--- hermes/settings.py
+++ hermes/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions'    ,
     'django.contrib.messages'    ,
     'django.contrib.staticfiles' ,
+    'django.contrib.humanize'    ,
 
     'companies',
 ]
@@ -84,11 +85,12 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-BR'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
+USE_L10N = True
 
 USE_TZ = True
 
