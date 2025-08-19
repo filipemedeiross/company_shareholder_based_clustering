8773909e093c9a133da40a0c23d41bad0711288c
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-08-19 15:46:01 +0000

    Refactoring the unit tests

--- companies/tests/test_views.py
+++ companies/tests/test_views.py
@@ -3,36 +3,55 @@ import unittest
 from django.test import Client
 from django.urls import reverse
 
-from companies.models import Companies
+from companies.models    import Companies
+from companies.constants import COMPANIES_LIST_PAGINATE, \
+                                COMPANIES_LIST_CONTEXT
 
 
 class TestHomeCompaniesView(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.url    = reverse('companies:list')
 
-    def test_first_page_returns_20_companies_sorted_by_cnpj(self):
-        response = self.client.get(reverse('companies:list'))
-
+    def test_first_page_returns_paginated_companies_sorted_by_cnpj(self):
+        response  = self.client.get(self.url)
         context   = response.context
-        companies = context['companies']
+        companies = context[COMPANIES_LIST_CONTEXT]
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn   ('companies', context)
-        self.assertIn   ('page_obj' , context)
+        self.assertEqual(response.status_code, 200    )
+        self.assertIn   ('page_obj'          , context)
         self.assertTrue (context['is_paginated'])
 
         cnpjs = [
             company.cnpj
             for company in companies
         ]
-
-        self.assertEqual(len(cnpjs)   , 20   )
-        self.assertEqual(sorted(cnpjs), cnpjs)
-
         expected_cnpjs = list(
             Companies
             .objects
-            .order_by('cnpj')
-            .values_list('cnpj', flat=True)[:20]
+            .order_by   ('cnpj')
+            .values_list('cnpj', flat=True)[:COMPANIES_LIST_PAGINATE]
         )
         self.assertEqual(cnpjs, expected_cnpjs)
+
+    def test_pagination_works_on_second_page(self):
+        response = self.client.get(self.url + '?page=2')
+        context  = response.context
+
+        self.assertEqual(context['page_obj'].number, 2)
+
+    def test_context_object_name_is_used(self):
+        response = self.client.get(self.url)
+
+        self.assertIn(COMPANIES_LIST_CONTEXT, response.context)
+
+    def test_template_used(self):
+        response = self.client.get(self.url)
+
+        template_names = [
+            t.name
+            for t in response.templates
+            if t.name
+        ]
+
+        self.assertIn('companies/list.html', template_names)
--- companies/views.py
+++ companies/views.py
@@ -1,15 +1,17 @@
 from django.views.generic import ListView
 
-from .models import Companies
+from .models    import Companies
+from .constants import COMPANIES_LIST_PAGINATE, \
+                       COMPANIES_LIST_CONTEXT
 
 
 class CompaniesListView(ListView):
     model = Companies
 
     template_name       = 'companies/list.html'
-    context_object_name = 'companies'
+    context_object_name = COMPANIES_LIST_CONTEXT
 
-    paginate_by = 20
+    paginate_by = COMPANIES_LIST_PAGINATE
     ordering    = [
         'cnpj'
     ]
