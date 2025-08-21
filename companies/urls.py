5452c57cc995bc07b35717a9a12bda4dd2cd03ea
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-08-21 15:35:08 +0000

    Refactoring the companies urls

--- companies/urls.py
+++ companies/urls.py
@@ -8,6 +8,6 @@ app_name = 'companies'
 
 
 urlpatterns = [
-    path(''               , CompaniesListView  .as_view(), name='list'  ),
-    path('search/<str:q>/', CompaniesSearchView.as_view(), name='search'),
+    path(''       , CompaniesListView  .as_view(), name='list'  ),
+    path('search/', CompaniesSearchView.as_view(), name='search'),
 ]
