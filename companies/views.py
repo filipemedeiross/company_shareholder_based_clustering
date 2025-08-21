108ac0ea7b04e7e0e3d111af06220a63fd6ba7ed
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-08-21 16:50:13 +0000

    Finish the companies list view refactoring and include the first version of companies search view

--- companies/views.py
+++ companies/views.py
@@ -1,3 +1,5 @@
+from django.db.models     import Q
+from django.http.response import Http404
 from django.views.generic import ListView
 
 from .models    import Companies
@@ -12,6 +14,23 @@ class CompaniesListView(ListView):
     context_object_name = COMPANIES_LIST_CONTEXT
 
     paginate_by = COMPANIES_LIST_PAGINATE
-    ordering    = [
-        'cnpj'
-    ]
+    ordering    = ['cnpj']
+
+
+class CompaniesSearchView(CompaniesListView):
+    template_name = 'companies/search.html'
+
+    def get_queryset(self):
+        q = self.request.GET.get('q', '')
+
+        if not q:
+            raise Http404
+
+        queryset = super().get_queryset()
+
+        return queryset.filter(
+            Q(
+                Q(cnpj__startswith         =q) |
+                Q(corporate_name__icontains=q)
+            )
+        )
