95034b924a6ebd4d4cc797433b838a1df8dbd441
Filipe de Medeiros <filipemedeiros2013@gmail.com>
2025-08-21 14:49:32 +0000

    Including the search URL in companies.urls

--- companies/urls.py
+++ companies/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from .views import CompaniesListView
+from .views import CompaniesListView,  \
+                   CompaniesSearchView
 
 
 app_name = 'companies'
 
 
 urlpatterns = [
-    path('', CompaniesListView.as_view(), name='list'),
+    path(''               , CompaniesListView  .as_view(), name='list'  ),
+    path('search/<str:q>/', CompaniesSearchView.as_view(), name='search'),
 ]
