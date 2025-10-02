97ab47e0147d99c48cbfa62dcc4798fcd55c69c1
filipemedeiross <filipemedeiros2013@gmail.com>
2025-10-02 20:14:27 -0300

    Fix the functional tests of summarizer app

--- functional_tests/test_summarizer.py
+++ functional_tests/test_summarizer.py
@@ -4,12 +4,12 @@ from .base import FunctionalTestBase
 
 
 class DashboardSummarizerTests(FunctionalTestBase):
-    base_url = reverse("summarizer:dashboard")
+    summarizer_url = reverse("summarizer:dashboard")
 
     def test_dashboard_view_loads_in_browser(self):
-        self.browser.get(self.base_url)
+        self.open_home(self.summarizer_url)
         self.assertIn("Database Statistics", self.browser.page_source)
 
     def test_dashboard_template_rendered(self):
-        self.browser.get(self.base_url)
+        self.open_home(self.summarizer_url)
         self.assertTrue(self.find_css_element("h2"))
